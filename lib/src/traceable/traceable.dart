b74f822f3886ffe9b4c32045a64228e5e2bd019d
Pau Picas <p.picas@stuart.com>
2024-12-11 08:20:58 +0100

    Add comments to Traceable

--- lib/src/traceable/traceable.dart
+++ lib/src/traceable/traceable.dart
@@ -1,5 +1,18 @@
+/// An interface that represents a traceable error.
+///
+/// This interface can be implemented by errors and exceptions that need to
+/// provide information about the causing error and its associated
+/// stack traces.
 abstract interface class Traceable {
+  /// The error or exception that caused the current issue.
+  ///
+  /// This property provides a detailed explanation of what went wrong
+  /// and is typically an exception or error object.
   Object get causeError;
 
+  /// The stack trace associated with the cause of the error.
+  ///
+  /// This property provides a snapshot of the call stack at the point where
+  /// the error or exception occurred, aiding in debugging and diagnostics.
   StackTrace get causeStackTrace;
 }
