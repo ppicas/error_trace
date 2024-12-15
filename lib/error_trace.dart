503d22355902ca6ee0dfa4ac31d6c932bbca24fb
Pau Picas <p.picas@stuart.com>
2024-12-15 17:39:47 +0100

    Move `traceable` folder files to the root

--- lib/error_trace.dart
+++ lib/error_trace.dart
@@ -3,7 +3,7 @@
 /// More dartdocs go here.
 library;
 
-export 'src/traceable/traceable.dart';
-export 'src/traceable/traceables.dart';
+export 'src/traceable.dart';
+export 'src/traceables.dart';
 export 'src/stack_trace_extension.dart';
 export 'src/error_formatting.dart';
--- lib/src/traceables.dart
+++ lib/src/traceables.dart
@@ -1,4 +1,4 @@
-import 'package:error_trace/src/traceable/traceable.dart';
+import 'package:error_trace/src/traceable.dart';
 
 class TraceableException implements Exception, Traceable {
   TraceableException(
