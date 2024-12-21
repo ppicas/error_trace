9667d7decca120e301106f3713645d60c831fd55
Pau Picas <p.picas@stuart.com>
2024-12-21 16:05:59 +0100

    Add documentation comments

--- lib/src/stack_trace_extension.dart
+++ lib/src/stack_trace_extension.dart
@@ -1,7 +1,37 @@
 import 'package:error_trace/error_trace.dart';
 import 'package:stack_trace/stack_trace.dart';
 
+/// Extension on the [StackTrace] class to provide functionality for chaining
+/// together multiple stack traces from a chain of errors.
 extension StackTraceExtension on StackTrace {
+  /// Chains together the stack traces of multiple errors.
+  ///
+  /// This method takes an [error] object and recursively traverses the chain
+  /// of errors, collecting the stack trace at each step. It expects errors
+  /// to implement the [Traceable] interface to provide access to the cause
+  /// error and its stack trace.
+  ///
+  /// If the passed [error] does not implement [Traceable], it will be
+  /// considered as the last error in the chain, and only the current
+  /// stack trace will be included in the result.
+  ///
+  /// The method returns a [Chain] object that contains the ordered list of
+  /// [Trace] objects, each representing a single stack trace in the chain.
+  ///
+  /// Example:
+  /// ```dart
+  /// FlutterError.onError = (error, stack) {
+  ///   // Records the unhandled error using Firebase Crashlytics
+  ///   FirebaseCrashlytics.instance.recordFlutterFatalError(
+  ///     error,
+  ///     // Creates a [Chain] instance that includes all the causes chained.
+  ///     //
+  ///     // [Chain] is a class provided by `stack_trace` package that
+  ///     // implements [StackTrace] so it can be passed to Crashlytics.
+  ///     stack.chainCauses(error),
+  ///   );
+  /// };
+  /// ```
   Chain chainCauses(Object error) {
     final traces = [Trace.from(this)];
 
--- lib/src/traceables.dart
+++ lib/src/traceables.dart
@@ -1,6 +1,28 @@
 import 'package:error_trace/src/traceable.dart';
 
+/// [TraceableException] is an [Exception] that implements the
+/// [Traceable] interface.
+///
+/// This allows it to be part of a chain of errors, where each error can
+/// point to the error that caused it, along with the corresponding stack trace.
+///
+/// You can use this class directly to create exceptions that can be chained,
+/// or you can extend it to create custom exception types that also support
+/// chaining.
 class TraceableException implements Exception, Traceable {
+  /// Creates a [TraceableException].
+  ///
+  /// The [causeError] is the error that caused this error.
+  ///
+  /// The [causeStackTrace] is the stack trace of the error that caused this
+  /// error.
+  ///
+  /// The [name] is an optional name for this error. It is used to format the
+  /// string when [toString] is called. It represents the name of the error.
+  ///
+  /// The [message] is an optional message for this error. It is used to format
+  /// the string when [toString] is called. It is a custom message explaining
+  /// the reason for the error.
   TraceableException(
     this.causeError,
     this.causeStackTrace, {
@@ -8,13 +30,20 @@ class TraceableException implements Exception, Traceable {
     this.message,
   });
 
+  /// An optional name for this error. It is used to format the string when
+  /// [toString] is called. It represents the name of the error.
   final String? name;
 
+  /// An optional message for this error. It is used to format the string when
+  /// [toString] is called. It is a custom message explaining the reason for
+  /// the error.
   final String? message;
 
+  /// The error that caused this error.
   @override
   final Object causeError;
 
+  /// The stack trace of the error that caused this error.
   @override
   final StackTrace causeStackTrace;
 
@@ -24,7 +53,27 @@ class TraceableException implements Exception, Traceable {
   }
 }
 
+/// [TraceableError] is an [Error] that implements the [Traceable] interface.
+///
+/// This allows it to be part of a chain of errors, where each error can
+/// point to the error that caused it, along with the corresponding stack trace.
+///
+/// You can use this class directly to create errors that can be chained, or
+/// you can extend it to create custom error types that also support chaining.
 class TraceableError extends Error implements Traceable {
+  /// Creates a [TraceableError].
+  ///
+  /// The [causeError] is the error that caused this error.
+  ///
+  /// The [causeStackTrace] is the stack trace of the error that caused this
+  /// error.
+  ///
+  /// The [name] is an optional name for this error. It is used to format the
+  /// string when [toString] is called. It represents the name of the error.
+  ///
+  /// The [message] is an optional message for this error. It is used to format
+  /// the string when [toString] is called. It is a custom message explaining
+  /// the reason for the error.
   TraceableError(
     this.causeError,
     this.causeStackTrace, {
@@ -32,13 +81,20 @@ class TraceableError extends Error implements Traceable {
     this.message,
   });
 
+  /// An optional name for this error. It is used to format the string when
+  /// [toString] is called. It represents the name of the error.
   final String? name;
 
+  /// An optional message for this error. It is used to format the string when
+  /// [toString] is called. It is a custom message explaining the reason for
+  /// the error.
   final String? message;
 
+  /// The error that caused this error.
   @override
   final Object causeError;
 
+  /// The stack trace of the error that caused this error.
   @override
   final StackTrace causeStackTrace;
 
