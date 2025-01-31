6603a7d6e60764cde8d0b1c3f3523e015ae0fc4d
Pau Picas <p.picas@stuart.com>
2025-01-31 07:44:30 +0100

    Improve async function example

--- example/trace_errors_from_async_function_example.dart
+++ example/trace_errors_from_async_function_example.dart
@@ -6,43 +6,42 @@ import 'fakes/network_fakes.dart';
 
 Future<void> main() async {
   try {
-    // Calls a function that calls `fakeNetworkOperation()` without catching
+    // Calls a function that calls `fakeNetworkOperation()` without chaining
     // possible exceptions.
-    await callWithoutThrowingTraceable();
+    await fetchData(chainExceptions: false);
   } catch (error, stackTrace) {
-    // In this case 'stackTrace' only contains the call to
-    // `fakeNetworkOperation()` function and doesn't contain
-    // `callWithoutThrowingTraceable()` or `main()` functions calls.
-    // This makes hard to know when `fakeNetworkOperation()` was called.
-    print('### Error without throwing a Traceable:');
+    // In this case the printed stack trace only contains the call to
+    // `fakeNetworkOperation()` function and doesn't contain `fetchData()` or
+    // `main()` functions calls. This makes hard to know when
+    // `fakeNetworkOperation()` was called.
+    print('### Error without chaining exceptions:');
     printError(error, stackTrace);
     print('');
   }
 
   try {
-    // Calls a function that calls `fakeNetworkOperation()` catching possible
-    // exceptions and throwing a `Traceable`.
-    await callAndThrowTraceable();
+    // Calls a function that calls `fakeNetworkOperation()` chaining possible
+    // exceptions by throwing a `Traceable`.
+    await fetchData(chainExceptions: true);
   } catch (error, stackTrace) {
-    // In this case `stackTrace` contains all the calls to
-    // `fakeNetworkOperation()`, `callAndThrowTraceable()`, and `main()`
-    // functions. This makes easy to know when `fakeNetworkOperation()` was
-    // called.
-    print('### Error throwing a Traceable:');
+    // In this case the printed stack trace contains all the calls to
+    // `fakeNetworkOperation()`, `fetchData()`, and `main()` functions. This
+    // makes easy to know when `fakeNetworkOperation()` was called.
+    print('### Error chaining exceptions by throwing a Traceable:');
     printError(error, stackTrace);
     print('');
   }
 }
 
-Future<void> callWithoutThrowingTraceable() async {
-  return fakeNetworkOperation(fail: true);
-}
-
-Future<void> callAndThrowTraceable() async {
-  try {
-    await fakeNetworkOperation(fail: true);
-  } catch (e, st) {
-    throw NetworkException(e, st);
+Future<void> fetchData({required bool chainExceptions}) async {
+  if (chainExceptions) {
+    try {
+      await fakeNetworkOperation(fail: true);
+    } catch (e, st) {
+      throw NetworkException(e, st);
+    }
+  } else {
+    return fakeNetworkOperation(fail: true);
   }
 }
 
