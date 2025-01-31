9feccfff3699aec08adb0ef0c45b789d9310f931
Pau Picas <p.picas@stuart.com>
2025-01-31 09:34:46 +0100

    Improve unawaited future example

--- example/trace_errors_of_unawaited_future_example.dart
+++ example/trace_errors_of_unawaited_future_example.dart
@@ -8,27 +8,28 @@ import 'fakes/network_fakes.dart';
 
 void main() {
   runZonedGuarded(() {
-    // Calls `fakeNetworkOperation()` without capturing the errors.
+    // Calls `fakeNetworkOperation()` without chaining possible exceptions
     fakeNetworkOperation(fail: true);
   }, (error, stackTrace) {
     // In this case 'stackTrace' only contains the call to
-    // `fakeNetworkOperation()` function and doesn't contain
-    // any `main()` functions calls.
-    // This makes hard to know when `fakeNetworkOperation()` was called.
-    print('### Uncaught error (Without error capturing):');
+    // `fakeNetworkOperation()` function and doesn't contain any `main()`
+    // functions calls. This makes hard to know when `fakeNetworkOperation()`
+    // was called.
+    print('### Uncaught error (Without chaining exceptions):');
     printError(error, stackTrace);
     print('');
   });
 
   runZonedGuarded(() {
-    // Calls `fakeNetworkOperation()`, but this time capturing the errors.
+    // Calls `fakeNetworkOperation()` chaining possible exceptions by throwing a
+    // `Traceable`.
     fakeNetworkOperation(fail: true)
         .catchError((e, st) => throw NetworkException(e, st));
   }, (error, stackTrace) {
     // In this case `stackTrace` contains all the calls to
-    // `fakeNetworkOperation()`, and `main()` functions.
-    // This makes easy to know when `fakeNetworkOperation()` was called.
-    print('### Uncaught error (With error capturing):');
+    // `fakeNetworkOperation()`, and `main()` functions. This makes easy to know
+    // when `fakeNetworkOperation()` was called.
+    print('### Uncaught error (Chaining exceptions by throwing a Traceable):');
     printError(error, stackTrace);
     print('');
   });
